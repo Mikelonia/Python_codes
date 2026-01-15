bf3333b6fd074567e38073f092329f8c8922638e
Mikelonia <onajitemike4real@gmail.com>
2026-01-15 20:49:06 +0000

    List ran successfully

---
+++ list.py
@@ -0,0 +1,10 @@
+#Simple use of list, to test my knowledge of list
+
+Fruits = {"Apple", "Orange", "Pineapple", "Pawpaw", "Mango"}
+def getmyfavs(Fruits):
+	for fruit in fruits:
+		print(f"Mike loves eating {fruit}")
+getmyfavs(Fruits)
+print(f"thank you, you now know my favorite fruits".upper())
+#Addition of .upper method to print the output in uppercase 
+
